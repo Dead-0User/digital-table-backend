cafeff8dd2c9006b6eda34798a1e3a7fd5d865c2
Uditanshu Bhatt <uditanshubhatt@gmail.com>
2026-01-05 10:38:57 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,7 +26,8 @@ const allowedOrigins = [
   'https://abhu.duckdns.org',
   'http://localhost:3000',
   'http://localhost:5173',
-  'http://localhost:5174',
+  'http://localhost:5174'
+   'http://localhost:8080',
 ];
 
 const corsOptions = {
